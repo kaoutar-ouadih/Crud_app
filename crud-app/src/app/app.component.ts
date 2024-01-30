824b2818488eab635dba323453931dee6f2b1a16
kaoutarOuadih1 <kaoutar.ouadih8@gmail.com>
2024-01-30 16:04:43 +0100

    Add new employee modal form with data binding

--- crud-app/src/app/app.component.ts
+++ crud-app/src/app/app.component.ts
@@ -4,14 +4,26 @@ import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule ],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'crud-app';
+
+  constructor(private _dialog : MatDialog){
+  }
+
+  openAddEditEmpForm(){
+    this._dialog.open(EmpAddEditComponent);
+  }
+
 }
--- crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
+++ crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-emp-add-edit',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatRadioModule, MatSelectModule, ReactiveFormsModule ],
   templateUrl: './emp-add-edit.component.html',
   styleUrl: './emp-add-edit.component.css'
 })
 export class EmpAddEditComponent {
+  education : string[] =['Bac', 'Bac+2', 'Bac+3', 'Back+5'];
+
+  empGroup : FormGroup;
+
+  constructor(private _fb : FormBuilder){
+    this.empGroup = this._fb.group({
+      firstName : '',
+      lastName : '',
+      email : '',
+      dateOfBirth : '',
+      gender : '',
+      education : '',
+      company :''
+    })
+  }
+  OnSubmitForm(){
+    console.log(this.empGroup.value);
+  }
 
 }
