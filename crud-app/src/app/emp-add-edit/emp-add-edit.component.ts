e1d49800fc7f4cd755fb20f6ccfe5d57a6c0c221
kaoutarOuadih1 <kaoutar.ouadih8@gmail.com>
2024-01-30 12:45:08 +0100

    the structure of the project

---
+++ crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './emp-add-edit.component.html',
+  styleUrl: './emp-add-edit.component.css'
+})
+export class EmpAddEditComponent {
+
+}
